2857602a669d5334c7292f5330ef304e2e1c2947
David Bribiesca <dabrival03@gmail.com>
2025-01-04 15:37:25 -0600

    Minor changes

--- projects/01-Connect-four/src/App.jsx
+++ projects/01-Connect-four/src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css'
 import { useState } from 'react'
 
-const Square = ({children}) => {
+const Square = ({children, index}) => {
   return(
     <div>
       {children}
@@ -10,8 +10,8 @@ const Square = ({children}) => {
 }
 
 const Arrow = ({index, updateBoard}) => {
-  const handleClick = ({index}) => {
-    console.log({index})
+  const handleClick = () => {
+    updateBoard({index})
   }
   return(
     <img src="public\arrow.png" onClick={handleClick}/>
@@ -24,23 +24,30 @@ function App() {
   const [board, setBoard] = useState(Array(42).fill(null))
   const arrows = Array(7).fill(null)
 
+  const updateBoard = ({index}) => {
+    let newBoard = board.slice()
+    console.log(newBoard)
+
+  }
+
   return (
     <>
       <header>
         <h1>Connect 4</h1>
-
       </header>
       <section className='arrows'>
         {arrows.map((_, index) =>{
           return(
-            <Arrow key={index} />
+            <Arrow key={index} index={index} updateBoard={updateBoard}/>
           )
         })}
       </section>
       <section className="board">
         {board.map((_, index) =>{
           return(
-            <Square key={index} index={index}/>
+            <Square key={index} index={index} >
+              {board[index]}
+            </Square>
           )
         })}
       </section>
