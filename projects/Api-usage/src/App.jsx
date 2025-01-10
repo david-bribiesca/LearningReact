7436b217cc242a7383e330ab03152388eee7b5be
David Bribiesca <dabrival03@gmail.com>
2025-01-09 19:45:17 -0600

    finished fetching

--- projects/Api-usage/src/App.jsx
+++ projects/Api-usage/src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
+const CAT_PREFIX_IMAGE_URL = 'https://cataas.com'
 
+function App() {
+  const [fact, setFact]=useState()
+  const [imageUrl, setImageUrl] = useState()
+
+  useEffect(()=>{
+    fetch(CAT_ENDPOINT_RANDOM_FACT)
+      .then(res => res.json())
+      .then(data => {
+        const {fact} = data
+        setFact(fact)
+
+        const threeFirstWords = fact.split(' ', 3).join(' ')
+        fetch(`https://cataas.com/cat/says/${threeFirstWords}?size=50&fontColor=red&json=true`)
+          .then(res => res.json())
+          .then(response => {
+            const {_id} = response
+            const url = `/cat/${_id}/says/${threeFirstWords}`
+            setImageUrl(url)
+          })
+      })
+  }, [])
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <h1>Gatitos</h1>
+        {fact && <p>{fact}</p>}
+        {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words from`}/>}
+      </main>
     </>
   )
 }
